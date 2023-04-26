4b261500c520cd5014557f084c9ffa2b64353ef0
Quique Ciria <quiqueciria@gmail.com>
2023-04-26 12:03:11 +0200

    converted to TS

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,69 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+// ----------------- COUNT LESS
+function contar() {
+  //IT READS THE COUNTER
+  const marcador: string = document.getElementById("numeroTurno")
+    ?.innerText as string;
+
+  //APPLY DECRECENT
+  const countMinus = parseInt(marcador) - 1;
+
+  //SHOW RESULT
+  const numeroTurnoElement = document.getElementById("numeroTurno");
+
+  if (numeroTurnoElement !== null && numeroTurnoElement !== undefined) {
+    numeroTurnoElement.innerHTML = countMinus.toString();
+  }
+}
+
+//CALL TO THE FUNCTION
+const botonAnterior = document.getElementById("anterior");
+
+if (botonAnterior !== null && botonAnterior !== undefined) {
+  botonAnterior.addEventListener("click", contar);
+}
+
+// ----------------- COUNT MORE
+function contarPlus() {
+  //IT READS THE COUNTER
+  const marcadorPlus: string = document.getElementById("numeroTurno")
+    ?.innerText as string;
+
+  //APPLY DECRECENT
+  const countPlus = parseInt(marcadorPlus) + 1;
+
+  //SHOW RESULT
+  const numeroTurnoElement = document.getElementById("numeroTurno");
+
+  if (numeroTurnoElement !== null && numeroTurnoElement !== undefined) {
+    numeroTurnoElement.innerHTML = countPlus.toString();
+  }
+}
+
+//CALL TO THE FUNCTION
+const botonSiguiente = document.getElementById("siguiente");
+
+if (botonSiguiente !== null && botonSiguiente !== undefined) {
+  botonSiguiente.addEventListener("click", contarPlus);
+}
+
+// ----------------- RESET
+function reset() {
+  //APPLY DECRECENT
+  const countReset = 0;
+
+  //SHOW RESULT
+  const numeroTurnoElement = document.getElementById("numeroTurno");
+
+  if (numeroTurnoElement !== null && numeroTurnoElement !== undefined) {
+    numeroTurnoElement.innerHTML = countReset.toString();
+  }
+}
+
+//CALL TO THE FUNCTION
+const botonReset = document.getElementById("reset");
+
+if (botonReset !== null && botonReset !== undefined) {
+  botonReset.addEventListener("click", reset);
+}
